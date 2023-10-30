75e734599a32d7e665676c08b921c16a6456a4d4
niketzaBG <90557280+niketzaBG@users.noreply.github.com>
2023-10-30 20:11:29 +0200

    Add files via upload

---
+++ RandomList/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList rand = new RandomList();
+            rand.Add("a");
+            rand.Add("b");
+            rand.Add("c");
+            rand.Add("d");
+
+            Console.WriteLine("The removed random value is:");
+            Console.WriteLine(rand.RandomString());
+
+            Console.WriteLine(String.Join(", ", rand));
+        }
+    }
+}
